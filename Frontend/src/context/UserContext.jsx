067e95c5fe762424399b13f5280b53635933e7ea
harshshukla <talktoharshshukla@gmail.com>
2025-12-02 23:47:18 +0530

    user context initialize

---
+++ Frontend/src/context/UserContext.jsx
@@ -0,0 +1,25 @@
+import React , {createContext} from 'react'
+import { useState } from 'react'
+
+const UserDataContext = createContext()
+
+const UserContext = ({children}) => {
+
+    const [user, setUser] = useState({
+        email: '',
+        fullname: {
+            firstname: '',
+            lastname: ''
+        }
+    })
+
+  return (
+    <UserDataContext.Provider value={[user, setUser]}>
+      {children}
+    </UserDataContext.Provider>
+  )
+
+
+}
+
+export default UserContext
