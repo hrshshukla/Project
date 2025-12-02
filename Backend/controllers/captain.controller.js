7482666e8a1645b48a8f817e6d8cd40a746e7e23
harshshukla <talktoharshshukla@gmail.com>
2025-12-02 19:24:06 +0530

    captain routes completed

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const captainModel = require("../models/captain.model");
 const captainService = require("../services/captain.service");
+const blackListTokenModel = require("../models/blacklistToken.model");
 const {validationResult} =  require("express-validator");
 
 module.exports.registerCaptain = async (req, res, next) => {
@@ -15,11 +16,9 @@ module.exports.registerCaptain = async (req, res, next) => {
     const { fullname, email, password, vehicle } = req.body;
 
     const isCaptainAlreadyExist = await captainModel.findOne({ email });
-
     if (isCaptainAlreadyExist) {
         return res.status(400).json({ message: 'Captain already exist' });
     }
-
 
     const hashedPassword = await captainModel.hashPassword(password);
 
@@ -37,4 +36,44 @@ module.exports.registerCaptain = async (req, res, next) => {
     const token = captain.generateAuthToken();
 
     res.status(201).json({ token, captain });
+}
+
+module.exports.loginCaptain = async (req, res, next) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const captain = await captainModel.findOne({ email }).select("+password");
+    if (!captain) {
+        return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await captain.comparePassword(password);
+    if (!isMatch) {
+        return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = captain.generateAuthToken();
+    res.cookie("token", token);
+    res.status(200).json({ token, captain });
+}
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+    res.status(200).json({captain: req.captain});
+}
+
+module.exports.logoutCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+    if (!token) {
+        return res.status(400).json({ message: "Unauthorized" });
+    }
+    
+    await blackListTokenModel.create({ token });
+
+    res.clearCookie("token");
+    res.status(200).json({ message: "Logged out successfully" });
 }
--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,8 +1,10 @@
-const userModel = require("../models/user.model");
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
+const captainModel = require("../models/captain.model");
 const blackListTokenModel = require("../models/blacklistToken.model");
+const bcrypt = require("bcrypt");
 
+// User Authentication Middleware
 module.exports.authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
@@ -24,6 +26,37 @@ module.exports.authUser = async (req, res, next) => {
         }
 
         req.user = user;
+        
+        next();
+
+    } catch (error) {
+        return res.status(401).json({message : "Unauthorized Access"});
+    }
+}
+
+// Captain Authentication Middleware
+module.exports.authCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+    if(!token) {
+        return res.status(401).json({message : "Unauthorized Access"});
+    }
+    
+    const isBlacklisted = await blackListTokenModel.findOne({token: token});
+    if(isBlacklisted) {
+        return res.status(401).json({message : "Unauthorized Access"});
+    }
+    
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const captain = await captainModel.findById(decoded._id);
+
+        if (!captain) {
+            return res.status(401).json({ message: "Unauthorized Access" });
+        }
+
+        req.captain = captain;
+        
         next();
 
     } catch (error) {
